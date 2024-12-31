d49e81bc23e8334ec43363c04a37bf1b050d663e
Yourzo <jurajkobenovy@gmail.com>
2024-12-31 03:18:54 +0100

    well it works, changes colors recursively and changes back when called again

--- src/main/kotlin/sk/zuray/shortcutlabeler/actions/ShowLabelsAction.kt
+++ src/main/kotlin/sk/zuray/shortcutlabeler/actions/ShowLabelsAction.kt
@@ -4,28 +4,43 @@ import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.wm.WindowManager
-import com.intellij.ui.JBColor
+import java.awt.Color
 import java.awt.Component
 import java.awt.Container
-import java.util.TreeMap
 import javax.swing.AbstractButton
 import javax.swing.JComponent
 import javax.swing.JMenuItem
 
 
 class ShowLabelsAction: AnAction() {
+    private val originalComponentColor = mutableMapOf<Component, Color>()
     override fun actionPerformed(e: AnActionEvent) {
+        if (originalComponentColor.isNotEmpty()) {
+            rollbackOriginalColors()
+            return
+        }
         val rp = ApplicationManager.getApplication().getService(WindowManager::class.java).getIdeFrame(e.project!!)?.component
         if (rp != null) {
             recursShowLabelsOnLeaves(rp)
         }
     }
 
+    private fun rollbackOriginalColors() {
+        for (component in originalComponentColor.keys) {
+            if (component is JComponent) {
+                component.isOpaque = true
+            }
+            component.background = originalComponentColor[component]
+        }
+        originalComponentColor.clear()
+    }
+
     private fun showLabelsOnComponent(component: Component) {
+        originalComponentColor[component] = component.background
         if (component is JComponent) {
             component.isOpaque = true
         }
-        component.background = JBColor.YELLOW
+        component.background = Color.YELLOW
     }
 
     private fun componentIsClickable(component: Component): Boolean {
@@ -56,8 +71,6 @@ class ShowLabelsAction: AnAction() {
         }
         if (componentIsClickable(component)) {
             showLabelsOnComponent(component)
-            val id = component.name
-            println("Component:$id")
         }
     }
 }
